d5df69d2e41f34c85954ce70291ed8d2cb3b6b0a
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-21 01:17:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,12 @@
-def turn_count
+def turn_count(board)
+  num_turns = 0
+
+  board.each do |val|
+    if val == 'X' || val == 'O'
+      num_turns += 1
+    end
+  end
 end
 
-def current_player
+def current_player(board)
 end
